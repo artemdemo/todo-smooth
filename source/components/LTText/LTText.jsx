845719a3daa963986979a4b413953ce3a6d663e0
Artem Demo <artemdemo@gmail.com>
2017-09-27 09:34:11 +0300

    problem with animation (see descr. in commit)
    The propblem is that it looks like preact first rearanging text and then removeing <div />.
    That means that animaition class is still presented after text rearangment, and next element having line-through animation.

--- source/components/LTText/LTText.jsx
+++ source/components/LTText/LTText.jsx
@@ -4,20 +4,48 @@ import classnames from 'classnames';
 
 import './LTText.less';
 
-const LTText = (props) => {
-    const { lineThrough, className } = props;
-    const wrapClass = classnames(className, 'lt-text-wrap');
-    const ltClass = classnames({
-        'lt-text-line': true,
-        'lt-text-line_show': lineThrough,
-    });
-    return (
-        <div className={wrapClass}>
-            <div className={ltClass} />
-            {props.children}
-        </div>
-    );
-};
+class LTText extends Preact.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animated: true,
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.lineThrough !== nextProps.lineThrough) {
+            this.setState({
+                animated: true,
+            });
+        }
+    }
+
+    transitionEnded() {
+        console.log('transitionEnded');
+        this.setState({
+            animated: false
+        });
+    }
+
+    render(props, state) {
+        const { lineThrough, className } = props;
+        const wrapClass = classnames(className, 'lt-text-wrap');
+        const ltClass = classnames({
+            'lt-text-line': true,
+            'lt-text-line_animated': state.animated,
+            'lt-text-line_show': lineThrough,
+        });
+        return (
+            <div className={wrapClass}>
+                <div
+                    className={ltClass}
+                    onTransitionEnd={this.transitionEnded.bind(this)}
+                />
+                {props.children}
+            </div>
+        );
+    }
+}
 
 LTText.propTypes = {
     lineThrough: PropTypes.bool,
