a32ec91e4b5d253e288790cd4b97aa5a4a0562e1
Artem Demo <artemdemo@gmail.com>
2017-09-29 15:41:28 +0300

    line through animation

--- source/components/LTText/LTText.jsx
+++ source/components/LTText/LTText.jsx
@@ -5,17 +5,52 @@ import classnames from 'classnames';
 import './LTText.less';
 
 class LTText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lineWidth: 0,
+            animated: false,
+        };
+
+        this.textRef = null;
+    }
+
+    setTextRef(ref) {
+        this.textRef = ref;
+    }
+
+    componentDidMount() {
+        this.setState({
+            lineWidth: this.textRef.offsetWidth,
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.lineThrough !== nextProps.lineThrough) {
+            this.setState({
+                animated: true,
+            });
+        }
+    }
+
     render() {
         const { lineThrough, className } = this.props;
         const wrapClass = classnames(className, 'lt-text-wrap');
         const ltClass = classnames({
             'lt-text-line': true,
-            'lt-text-line_show': lineThrough,
+            'lt-text-line_animated': this.state.animated,
         });
         return (
             <div className={wrapClass}>
-                <div className={ltClass} />
-                {this.props.children}
+                <div
+                    className={ltClass}
+                    style={{
+                        width: lineThrough ? this.state.lineWidth : 0,
+                    }}
+                />
+                <span ref={this.setTextRef.bind(this)}>
+                    {this.props.children}
+                </span>
             </div>
         );
     }
